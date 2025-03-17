c127133a97baae72a7d51ceb78a1a28014bd3e07
Al Fatih Muzakki <malfazakki@gmail.com>
2025-03-17 23:53:04 +0700

    feat: implement EmailTask Execute and Info Method

--- main.go
+++ main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type Task interface {
+	Execute() error
+	Info() string
+}
+
+type BaseTask struct {
+	ID          string
+	CreatedAt   time.Time
+	Description string
+}
+
+type EmailTask struct {
+	BaseTask
+}
+
+func (e *EmailTask) Execute() error {
+	fmt.Printf("Executing Email Task: %s\n", e.ID)
+
+	time.Sleep(time.Duration(rand.Intn(3)+1) * time.Second)
+
+	if rand.Float32() < .2 {
+		return fmt.Errorf("EmailTask %s failed", e.ID)
+	}
+
+	return nil
+}
+
+func (e *EmailTask) Info() string {
+	return fmt.Sprintf("EmailTask %s - %s", e.ID, e.Description)
+}
+
 func main() {
-	
+
 }
